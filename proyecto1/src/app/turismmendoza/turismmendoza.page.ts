cb3365436668ce36a8322f992ef67fc80ebc4d02
valentinorusso <valentino.russo02@gmail.com>
2021-10-29 00:04:05 -0300

    cambios

--- proyecto1/src/app/turismmendoza/turismmendoza.page.ts
+++ proyecto1/src/app/turismmendoza/turismmendoza.page.ts
@@ -11,5 +11,12 @@ export class TurismmendozaPage implements OnInit {
 
   ngOnInit() {
   }
+  option = {
+    slidesPerView: 1.5,
+    centerSlides: true, 
+    loop: true,
+    spaceBetween: 10,
+    autoplay: true,
 
+  }
 }
