11ebaec28d6c9acb5ad49e12c55e2c35b6d72f76
NachoBeltramo <ignaciobeltramo11@gmail.com>
2021-11-01 00:14:23 -0300

    mov imgs 2

--- proyecto1/src/app/shoppingrn/shoppingrn.page.ts
+++ proyecto1/src/app/shoppingrn/shoppingrn.page.ts
@@ -11,5 +11,12 @@ export class ShoppingrnPage implements OnInit {
 
   ngOnInit() {
   }
+  option = {
+    slidesPerView: 1.5,
+    centerSlides: true, 
+    loop: true,
+    spaceBetween: 10,
+    autoplay: true,
 
+  }
 }
